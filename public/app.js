9db8953141c305266d3b7defa133acf919d94bd4
Rodrigo Bestard Pino <rbestardpino@est.frba.utn.edu.ar>
2021-07-27 20:01:24 -0300

    funca?

--- public/app.js
+++ public/app.js
@@ -1,19 +1,19 @@
 const app = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    url: '',
-    slug: '',
-    error: '',
+    url: "",
+    slug: "",
+    error: "",
     formVisible: true,
     created: null,
   },
   methods: {
     async createUrl() {
-      this.error = '';
-      const response = await fetch('/url', {
-        method: 'POST',
+      this.error = "";
+      const response = await fetch("/url", {
+        method: "POST",
         headers: {
-          'content-type': 'application/json',
+          "content-type": "application/json",
         },
         body: JSON.stringify({
           url: this.url,
@@ -23,9 +23,10 @@ const app = new Vue({
       if (response.ok) {
         const result = await response.json();
         this.formVisible = false;
-        this.created = `https://cdg.sh/${result.slug}`;
+        this.created = `https://bep-sh.herokuapp.com//${result.slug}`;
       } else if (response.status === 429) {
-        this.error = 'You are sending too many requests. Try again in 30 seconds.';
+        this.error =
+          "You are sending too many requests. Try again in 30 seconds.";
       } else {
         const result = await response.json();
         this.error = result.message;
