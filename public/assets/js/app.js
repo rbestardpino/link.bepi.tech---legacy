c072452759f263fd437dc51fdb7335295780377f
Rodrigo Bestard Pino <rbestardpino@est.frba.utn.edu.ar>
2021-07-31 12:20:36 -0300

    i simply dont get it

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,42 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
   M.Modal.init(document.querySelectorAll('.modal'))
   M.Sidenav.init(document.querySelectorAll('.sidenav'))
-})
 
-document
-  .getElementById('submit-shorten-form')
-  .addEventListener('click', async () => {
-    const url = document.getElementById('url').value
-    const slug = document.getElementById('slug').value
+  document
+    .getElementById('submit-shorten-form')
+    .addEventListener('click', async () => {
+      const url = document.getElementById('url').value
+      const slug = document.getElementById('slug').value
 
-    if (!url) {
-      M.toast({ html: 'URL is required', classes: 'rounded red' })
-      return
-    }
+      if (!url) {
+        M.toast({ html: 'URL is required', classes: 'rounded red' })
+        return
+      }
 
-    const response = await fetch('/url', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        url: url,
-        slug: slug || undefined,
-      }),
+      const response = await fetch('/url', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          url: url,
+          slug: slug || undefined,
+        }),
+      })
+      let error = ''
+      if (response.ok) {
+        const result = await response.json()
+        document.getElementById(
+          'shortened'
+        ).innerHTML = `Shortened link: <a href="http://link.bepi.tech/${result.slug}">http://link.bepi.tech/${result.slug}</a>`
+      } else if (response.status === 429) {
+        error = 'You are sending too many requests. Try again in 30 seconds.'
+      } else {
+        const result = await response.json()
+        error = result.message
+      }
+      if (error) {
+        M.toast({ html: error, classes: 'rounded red' })
+      }
     })
-    let error = ''
-    if (response.ok) {
-      const result = await response.json()
-      document.getElementById(
-        'shortened'
-      ).innerHTML = `Shortened link: <a href="http://link.bepi.tech/${result.slug}">http://link.bepi.tech/${result.slug}</a>`
-    } else if (response.status === 429) {
-      error = 'You are sending too many requests. Try again in 30 seconds.'
-    } else {
-      const result = await response.json()
-      error = result.message
-    }
-    if (error) {
-      M.toast({ html: error, classes: 'rounded red' })
-    }
-  })
+})
